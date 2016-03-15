f412504070fb2307448a6ced15c8eea34399b774
Fabian Kugler <fk063@hdm-stuttgart.de>
2016-03-15 12:06:48 +0100

    added first ui outline - added LoginView - added MainView - added LoginViewController

---
+++ ios/app/occupyhdm/occupyhdm/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  occupyhdm
+//
+//  Created by Fabian Kugler on 15.03.16.
+//  Copyright © 2016 occupyhdm. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var usernamePassword: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
