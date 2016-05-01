e692d406c28c401970faf8746f1828e33fbdb943
Per Guth <mail@perguth.de>
2016-05-01 14:54:48 +0200

    node: more logging

--- node/index.js
+++ node/index.js
@@ -1,20 +1,26 @@
-var restify = require('restify');
+var restify = require('restify')
+
+require('console-stamp')(console)
 
 var server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+  name: 'OccupyHdM-REST-backend',
+  version: '0.0.0'
+})
+server.use(restify.acceptParser(server.acceptable))
+server.use(restify.queryParser())
+server.use(restify.bodyParser())
 
-var goals = { "locations" : [ { "name" : "Location 1", "lat" : 48.742070, "lon" : 9.102263 }, { "name" : "Location 2", "lat" : 48.740995, "lon" : 9.101709 } ] }
+var goals = {'locations': [
+  {'name': 'Location 1', 'lat': 48.742070, 'lon': 9.102263},
+  {'name': 'Location 2', 'lat': 48.740995, 'lon': 9.101709}
+]}
 
 server.get('/goals', function (req, res, next) {
-  res.send(goals);
-  return next();
-});
+  console.log('received a request on /goals')
+  res.send(goals)
+  return next()
+})
 
 server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+  console.log('%s listening at %s', server.name, server.url)
+})
