0cb4b6b83abcbc2f1ced5dda47b07bc771043557
Fabian Kugler <fk063@hdm-stuttgart.de>
2016-05-10 12:03:23 +0200

    linked ui to code for LoginActivity - Show/hide warning if username field is empty.

--- android/app/OccupyHdM/app/src/main/java/occupyhdm/occupyhdm/LoginActivity.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/occupyhdm/LoginActivity.java
@@ -2,10 +2,17 @@ package occupyhdm.occupyhdm;
 
 import android.app.Activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+    EditText editTextUsername;
+    TextView textViewUsernameWarning;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -13,6 +20,19 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        textViewUsernameWarning = (TextView) findViewById(R.id.textViewUsernameWarning);
+    }
+
+    public void startGame (View view) {
+        String username = editTextUsername.getText().toString();
+
+        if (username.equals("")) {
+            textViewUsernameWarning.setVisibility(View.VISIBLE);
+        } else {
+            textViewUsernameWarning.setVisibility(View.INVISIBLE);
+        }
     }
 }
 
