ddaa3c29fbb93920629b69cb217a075b443ba16d
Per Guth <mail@perguth.de>
2016-05-17 15:15:31 +0200

    render map & add overlay

--- android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
@@ -1,6 +1,9 @@
 package occupyhdm.app;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -17,13 +21,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         Log.d("View", "MainActivity");
-        //MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
-        //mapFragment.getMapAsync(this);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
+        Log.d("onMapRead", "Callback received");
+        LatLng sydney = new LatLng(48.74207, 9.102263);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.setMapType(map.MAP_TYPE_SATELLITE);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16.85f));
     }
 
     @Override
