4c71b2a26337ca491df857f5126be95d129cf616
Per Guth <mail@perguth.de>
2016-05-17 14:27:06 +0200

    add GoogleMap to MainView - Add API key. - Include Google Play Services.

--- android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
@@ -1,26 +1,36 @@
 package occupyhdm.app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import com.google.android.gms.maps.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("View", "MainActivity");
+        //MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+        //mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-
+        super.onCreateOptionsMenu(menu);
         return true;
     }
 
