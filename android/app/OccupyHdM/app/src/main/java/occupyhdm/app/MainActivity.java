590365b5fe561d646d11f027dd77878b40cad17e
Fabian Kugler <fk063@hdm-stuttgart.de>
2016-05-17 12:36:35 +0200

    added SettingsActivity & added OptionsMenu to MainActivity

--- android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/app/MainActivity.java
@@ -1,7 +1,12 @@
 package occupyhdm.app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent intentStartSettings = new Intent(this, SettingsActivity.class);
+            startActivity(intentStartSettings);
+        }
+
+        return true;
     }
 }
