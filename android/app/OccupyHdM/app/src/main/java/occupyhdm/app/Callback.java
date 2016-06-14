2f9bd818cb428c41dbfbf65d3aacad126038165a
Per Guth <mail@perguth.de>
2016-06-14 14:59:34 +0200

    Generified callback result type

--- android/app/OccupyHdM/app/src/main/java/occupyhdm/app/Callback.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/app/Callback.java
@@ -2,11 +2,11 @@ package occupyhdm.app;
 
 import java.util.concurrent.Callable;
 
-public abstract class Callback implements Callable<Void> {
-    String result;
+public abstract class Callback<T> implements Callable<Void> {
+    T result;
 
-    void setResult (String string) {
-        result = string;
+    void setResult (T result) {
+        this.result = result;
     }
 
     public abstract Void call();
--- android/app/OccupyHdM/app/src/main/java/occupyhdm/app/RestTask.java
+++ android/app/OccupyHdM/app/src/main/java/occupyhdm/app/RestTask.java
@@ -1,21 +1,16 @@
 package occupyhdm.app;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.Callable;
 
 public class RestTask extends AsyncTask<String, Void, String> {
-    Callback callback;
+    Callback<String> callback;
 
     public RestTask (Callback callback) {
         this.callback = callback;
